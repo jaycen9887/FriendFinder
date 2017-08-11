76a58addc62bd35b5577454696f05dc2f8ec5746
Jaycen <jaycen9887@gmail.com>
2017-08-11 16:11:24 -0400

    working

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 var express = require('express');
-var parser = require('body-parser');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App is listening on port: " + PORT);
+});
